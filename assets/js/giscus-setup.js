5df8b0bf6d7646626fae1743b6f9da2d95fa20b0
jpzhang-xtu <32028282+jpzhang-xtu@users.noreply.github.com>
2026-05-25 14:58:01 +0000

    Deploying to gh-pages from @ jpzhang-xtu/jpz@c6b277da889083cb89ba0930a66ac750f5716f87 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jpzhang-xtu/al-folio",
+    "data-repo": "jpzhang-xtu/jpz",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
