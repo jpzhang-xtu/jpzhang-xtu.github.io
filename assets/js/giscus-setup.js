13682b1bd71b455a27dfe5060813def3ff6e41a6
jpzhang-xtu <32028282+jpzhang-xtu@users.noreply.github.com>
2026-05-25 14:29:25 +0000

    Deploying to gh-pages from @ jpzhang-xtu/homepage@0045c1d5c2f02bf27bdd9c022f8dff965fb9233b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jpzhang-xtu/mypage",
+    "data-repo": "jpzhang-xtu/homepage",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
